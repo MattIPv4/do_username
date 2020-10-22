780f1cd3bdb8a81421e23f32ddb73017154436b4
MattIPv4 <me@mattcowley.co.uk>
2020-10-22 18:06:37 +0100

    Username generator

---
+++ lib/do_username.rb
@@ -0,0 +1,60 @@
+module DOUsername
+  SEA_CREATURES = %w(walrus seal fish shark clam coral whale crab lobster starfish eel dolphin squid jellyfish ray giantSquid shrimp mantaRay angler snorkler scubaDiver).freeze
+  SEA_OBJECTS = %w(boat ship submarine yacht dinghy raft kelp).freeze
+
+  DESCRIPTORS = %w(cute adorable lovable happy sandy).freeze
+  SIZE_DESCRIPTORS = %w(large big small).freeze
+  CREATURE_DESCRIPTORS= %w(swimming).freeze
+
+  COLORS = %w(blue blueGreen darkCyan electricBlue greenBlue lightCyan lightSeaGreen seaGreen turquoise aqua aquamarine teal cyan gray).freeze
+
+  extend self
+
+  def generate(max_size = 30)
+    # Choose a noun first
+    noun = random_noun
+
+    # Choose a descriptor
+    descriptor = random_descriptor(noun)
+
+    # Choose a color
+    color = random_color
+
+    # Convert to title case and remove whitespace
+    noun = format(noun)
+    descriptor = format(descriptor)
+    color = format(color)
+
+    # Combine to create username shorter than or equal to given length
+    if (descriptor + color + noun).length <= max_size
+      descriptor + color + noun
+    elsif (descriptor + noun).length <= max_size
+      descriptor + noun
+    elsif (color + noun).length <= max_size
+      descriptor + noun
+    else
+      noun[0...max_size]
+    end
+  end
+
+  private
+
+  def random_noun
+    (SEA_OBJECTS + SEA_CREATURES).sample
+  end
+
+  def random_descriptor(noun)
+    descriptors = DESCRIPTORS.dup
+    descriptors += SIZE_DESCRIPTORS unless noun.start_with?('giant')
+    descriptors += CREATURE_DESCRIPTORS if SEA_CREATURES.include?(noun)
+    descriptors.sample
+  end
+
+  def random_color
+    COLORS.sample
+  end
+
+  def format(string)
+    string[0].upcase + string[1..-1]
+  end
+end
