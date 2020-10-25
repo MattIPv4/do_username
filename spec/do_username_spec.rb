5ba5166aba528c8d3620f280727b7ddff747e40b
Matt (IPv4) Cowley <me@mattcowley.co.uk>
2020-10-25 20:52:55 +0000

    Improve specs (#12)
    * Tests for DOUsername#random_noun
    
    * Tests for DOUsername#random_descriptor
    
    * Tests for DOUsername#random_color
    
    * Tests for DOUsername#format
    
    * Update tests for DOUsername#generate
    
    * Tests for DOUsername#combine_username

--- spec/do_username_spec.rb
+++ spec/do_username_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe DOUsername do
-  describe '.generate' do
+  describe '#generate' do
     subject { described_class }
 
     # Please update this spec if module constants are updated
@@ -22,6 +22,10 @@ RSpec.describe DOUsername do
       expect(subject.generate.scan(/[A-Z]/).size).to be >= 3
     end
 
+    it 'responds with whitespace-free string' do
+      expect(subject.generate.scan(/\s/).size).to be 0
+    end
+
     context 'with noun part' do
       before { stub_const('DOUsername::SEA_LIST', ['walrus']) }
 
@@ -32,12 +36,12 @@ RSpec.describe DOUsername do
 
     context 'with descriptor part' do
       before do
-        stub_const('DOUsername::DESCRIPTORS', %w[cute huge])
-        stub_const('DOUsername::CREATURE_DESCRIPTORS', ['swimming'])
+        stub_const('DOUsername::DESCRIPTORS', ['cute'])
+        stub_const('DOUsername::SEA_CREATURES', [])
       end
 
-      it 'has descriptor in generated username' do
-        expect(subject.generate).to match(/Cute|Huge|Swimming/)
+      it 'starts with a descriptor' do
+        expect(subject.generate).to start_with('Cute')
       end
     end
 
@@ -45,38 +49,36 @@ RSpec.describe DOUsername do
       before { stub_const('DOUsername::COLORS', ['blue']) }
 
       it 'contains a color in generated username' do
-        expect(subject.generate).to match(/Blue/)
+        expect(subject.generate).to include('Blue')
       end
     end
 
     context 'with max_size argument' do
-      it 'responds with valid username based on sent size' do
+      before do
+        stub_const('DOUsername::DESCRIPTORS', ['cute'])
+        stub_const('DOUsername::SEA_LIST', ['walrus'])
+        stub_const('DOUsername::COLORS', ['red'])
+        stub_const('DOUsername::SEA_CREATURES', [])
+      end
+
+      it 'responds with username shorter than or equal to given size' do
         expect(subject.generate(15).size).to be <= 15
       end
 
-      context 'when max_size is lower than the full combination' do
-        before do
-          stub_const('DOUsername::DESCRIPTORS', ['cute'])
-          stub_const('DOUsername::SEA_LIST', ['walrus'])
-          stub_const('DOUsername::COLORS', ['red'])
-          stub_const('DOUsername::SEA_CREATURES', [])
-        end
+      it 'responds with full combination when appropriate' do
+        expect(subject.generate(100)).to eq('CuteRedWalrus')
+      end
 
-        it 'responds with full combination (descriptor + color + noun)' do
-          expect(subject.generate(100)).to eq('CuteRedWalrus')
-        end
+      it 'responds with descriptor + noun when appropriate' do
+        expect(subject.generate(12)).to eq('CuteWalrus')
+      end
 
-        it 'responds with (descriptor + noun) username based' do
-          expect(subject.generate(12)).to eq('CuteWalrus')
-        end
+      it 'responds with color + noun when appropriate' do
+        expect(subject.generate(9)).to eq('RedWalrus')
+      end
 
-        it 'responds with (color + noun) username based' do
-          expect(subject.generate(9)).to eq('RedWalrus')
-        end
-
-        it 'responds with part of noun username' do
-          expect(subject.generate(5)).to eq('Walru')
-        end
+      it 'responds with part of the noun when appropriate' do
+        expect(subject.generate(5)).to eq('Walru')
       end
 
       context 'when is invalid' do
@@ -97,6 +99,128 @@ RSpec.describe DOUsername do
             subject.generate('abc')
           end.to raise_error(ArgumentError, 'The max_size argument must be an integer number greater than zero.')
         end
+      end
+    end
+  end
+
+  describe '#random_noun' do
+    subject { described_class }
+
+    before do
+      stub_const('DOUsername::SEA_LIST', ['walrus'])
+    end
+
+    it 'returns an item from the list of creatures and objects' do
+      expect(subject.send(:random_noun)).to eq('walrus')
+    end
+  end
+
+  describe '#random_descriptor' do
+    subject { described_class }
+
+    context 'when the noun is a sea object' do
+      # This doesn't completely test that this can't return a value from
+      # CREATURE_DESCRIPTORS, just that it returns a value from DESCRIPTORS
+      before do
+        stub_const('DOUsername::SEA_CREATURES', [])
+        stub_const('DOUsername::DESCRIPTORS', ['cute'])
+      end
+
+      it 'returns an item from the list of descriptors' do
+        expect(subject.send(:random_descriptor, 'walrus')).to eq('cute')
+      end
+    end
+
+    context 'when the noun is a sea creature' do
+      before do
+        stub_const('DOUsername::SEA_CREATURES', ['walrus'])
+      end
+
+      context 'with creature descriptors present' do
+        before do
+          stub_const('DOUsername::DESCRIPTORS', [])
+          stub_const('DOUsername::CREATURE_DESCRIPTORS', ['huge'])
+        end
+
+        it 'returns an item from the list of creature descriptors' do
+          expect(subject.send(:random_descriptor, 'walrus')).to eq('huge')
+        end
+      end
+
+      context 'with generic descriptors present' do
+        before do
+          stub_const('DOUsername::DESCRIPTORS', ['cute'])
+          stub_const('DOUsername::CREATURE_DESCRIPTORS', [])
+        end
+
+        it 'returns an item from the list of descriptors' do
+          expect(subject.send(:random_descriptor, 'walrus')).to eq('cute')
+        end
+      end
+    end
+  end
+
+  describe '#random_color' do
+    subject { described_class }
+
+    before do
+      stub_const('DOUsername::COLORS', ['red'])
+    end
+
+    it 'returns an item from the list of colors' do
+      expect(subject.send(:random_color)).to eq('red')
+    end
+  end
+
+  describe '#format' do
+    subject { described_class }
+
+    it 'sets the first character to be uppercase' do
+      expect(subject.send(:format, 'test')).to eq('Test')
+    end
+
+    context 'with a string with existing uppercase characters' do
+      it 'does not force existing characters to lowercase' do
+        expect(subject.send(:format, 'testTesting')).to eq('TestTesting')
+      end
+    end
+  end
+
+  describe '#combine_username' do
+    subject { described_class }
+
+    context 'when max_size allows for the full combination' do
+      it 'responds with full combination (descriptor + color + noun)' do
+        expect(subject.send(:combine_username, 100, 'Swimming', 'Red', 'Walrus'))
+          .to eq('SwimmingRedWalrus')
+      end
+    end
+
+    context 'when max_size allows for the descriptor and noun' do
+      it 'responds with the descriptor + noun combination' do
+        expect(subject.send(:combine_username, 14, 'Swimming', 'Red', 'Walrus'))
+          .to eq('SwimmingWalrus')
+      end
+    end
+
+    context 'when max_size allows for the color and noun' do
+      it 'responds with the colors + noun combination' do
+        expect(subject.send(:combine_username, 9, 'Swimming', 'Red', 'Walrus'))
+          .to eq('RedWalrus')
+      end
+    end
+
+    context 'when max_size allows for the noun' do
+      it 'responds with just the noun' do
+        expect(subject.send(:combine_username, 6, 'Swimming', 'Red', 'Walrus'))
+          .to eq('Walrus')
+      end
+    end
+
+    context 'when max_size is shorter than the noun' do
+      it 'responds with the noun trimmed to the max size' do
+        expect(subject.send(:combine_username, 4, 'Swimming', 'Red', 'Walrus'))
+          .to eq('Walr')
       end
     end
   end
