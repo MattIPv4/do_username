cd122e37bdb12ef280f4bfe5f572babd96ac2241
Shreyas Sreenivas <46835608+shreyas44@users.noreply.github.com>
2020-10-27 22:57:28 +0530

    Update generate.py to use upper case letters for name of contants and tuples so that it's immutable

--- python/do_username/generate.py
+++ python/do_username/generate.py
@@ -1,23 +1,20 @@
-import math
 import random
 
-sea_creatures = "walrus seal fish shark clam coral whale crab lobster starfish eel dolphin squid jellyfish ray shrimp mantaRay angler snorkler scubaDiver urchin anemone morel axolotl".split(" ")
-sea_objects = "boat ship submarine yacht dinghy raft kelp seaweed anchor".split(" ")
-adjective_descriptors = "cute adorable lovable happy sandy bubbly friendly floating drifting".split(" ")
-size_descriptors = "large big small giant massive tiny little yuge".split(" ")
-creature_descriptors = "swimming sleeping eating hiding".split(" ")
+SEA_CREATURES = tuple("walrus seal fish shark clam coral whale crab lobster starfish eel dolphin squid jellyfish ray shrimp mantaRay angler snorkler scubaDiver urchin anemone morel axolotl".split(" "))
+SEA_OBJECTS = tuple("boat ship submarine yacht dinghy raft kelp seaweed anchor".split(" "))
+ADJECTIVE_DESCRIPTORS = tuple("cute adorable lovable happy sandy bubbly friendly floating drifting".split(" "))
+SIZE_DESCRIPTION = tuple("large big small giant massive tiny little yuge".split(" "))
+CREATURE_DESCRIPTORS = tuple("swimming sleeping eating hiding".split(" "))
 
-sea_list = sea_creatures + sea_objects
-descriptors = adjective_descriptors + size_descriptors
+SEA_LIST = SEA_CREATURES + SEA_OBJECTS
+DESCRIPTORS = ADJECTIVE_DESCRIPTORS + SIZE_DESCRIPTION
 
-colors = "blue blueGreen darkCyan electricBlue greenBlue lightCyan lightSeaGreen seaGreen turquoise aqua aquamarine teal cyan gray darkBlue cerulean azure lapis navy".split(" ")
+COLORS = "blue blueGreen darkCyan electricBlue greenBlue lightCyan lightSeaGreen seaGreen turquoise aqua aquamarine teal cyan gray darkBlue cerulean azure lapis navy".split(" ")
 
-rand = lambda array: array[math.floor(random.random()*len(array))]
-
-random_noun = lambda: rand(sea_list)
-random_descriptor = lambda noun: rand(descriptors) if noun not in sea_creatures else rand(descriptors + creature_descriptors)
-random_color = lambda: rand(colors)
-format = lambda array: "".join(map(lambda word: word.title(), array))
+random_noun = lambda: random.choice(SEA_LIST)
+random_descriptor = lambda noun: random.choice(DESCRIPTORS) if noun not in SEA_CREATURES else random.choice(DESCRIPTORS + CREATURE_DESCRIPTORS)
+random_color = lambda: random.choice(COLORS)
+format = lambda array: "".join(map(lambda word: word[0].upper() + word[1:], array))
 
 def generate(max_size=30):
   noun = random_noun()
