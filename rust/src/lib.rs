18642ea52625587dd2a0428d79202683f4cd2f02
ThatCopy <62877244+ThatCopy@users.noreply.github.com>
2020-10-27 13:24:53 +0100

    Fix the vectors

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,6 @@
+#[macro_use(lazy_static)]
+extern crate lazy_static;
+
 pub mod do_username {
 	use rand::Rng;
 	fn to_title_case(s: &str) -> String {
@@ -20,33 +23,30 @@ pub mod do_username {
 		}
 		new_arr.join("")
 	}
-	pub const SEA_CREATURES: [&str; 24] = ["walrus", "seal", "fish", "shark", "clam", "coral", "whale", "crab", "lobster", "starfish", "eel", "dolphin", "squid", "jellyfish", "ray", "shrimp", "mantaRay", "angler", "snorkler", "scubaDiver", "urchin", "anemone", "morel", "axolotl"];
-	pub const SEA_OBJECTS: [&str; 8] = ["boatShip", "submarine", "yacht", "dinghy", "raft", "kelp", "seaweed", "anchor"];
-	pub const ADJECTIVE_DESCRIPTORS: [&str; 9] = ["cute", "adorable", "lovable", "happy", "sandy", "bubbly", "friendly", "floating", "drifting"];
-	pub const SIZE_DESCRIPTORS: [&str; 8] = ["large", "big", "small", "giant", "massive", "tiny", "little", "yuge"];
-	pub const CREATURE_DESCRIPTORS: [&str; 4] = ["swimming", "sleeping", "eating", "hiding"];
-	pub const COLORS: [&str; 19] = ["blue", "blueGreen", "darkCyan", "electricBlue", "greenBlue", "lightCyan", "lightSeaGreen", "seaGreen", "turquoise", "aqua", "aquamarine", "teal", "cyan", "gray", "darkBlue", "cerulean", "azure", "lapis", "navy"];
-	
+	lazy_static! {
+		pub static ref SEA_CREATURES: std::vec::Vec<&'static str> = vec!["walrus", "seal", "fish", "shark", "clam", "coral", "whale", "crab", "lobster", "starfish", "eel", "dolphin", "squid", "jellyfish", "ray", "shrimp", "mantaRay", "angler", "snorkler", "scubaDiver", "urchin", "anemone", "morel", "axolotl"];
+		pub static ref SEA_OBJECTS: std::vec::Vec<&'static str> = vec!["boatShip", "submarine", "yacht", "dinghy", "raft", "kelp", "seaweed", "anchor"];
+		pub static ref ADJECTIVE_DESCRIPTORS: std::vec::Vec<&'static str> = vec!["cute", "adorable", "lovable", "happy", "sandy", "bubbly", "friendly", "floating", "drifting"];
+		pub static ref SIZE_DESCRIPTORS: std::vec::Vec<&'static str> = vec!["large", "big", "small", "giant", "massive", "tiny", "little", "yuge"];
+		pub static ref CREATURE_DESCRIPTORS: std::vec::Vec<&'static str> = vec!["swimming", "sleeping", "eating", "hiding"];
+		pub static ref SEA_LIST: Vec<&'static str> = SEA_OBJECTS.iter().cloned().chain(SEA_CREATURES.iter().cloned()).collect();
+		pub static ref DESCRIPTORS: Vec<&'static str> = ADJECTIVE_DESCRIPTORS.iter().cloned().chain(SIZE_DESCRIPTORS.iter().cloned()).collect();
+		pub static ref COLORS: std::vec::Vec<&'static str> = vec!["blue", "blueGreen", "darkCyan", "electricBlue", "greenBlue", "lightCyan", "lightSeaGreen", "seaGreen", "turquoise", "aqua", "aquamarine", "teal", "cyan", "gray", "darkBlue", "cerulean", "azure", "lapis", "navy"];
+	}
 	pub fn generate(size: Option<usize>) -> String {
 		let max_size = size.unwrap_or(30);
-		let mut sea_list = SEA_OBJECTS.to_vec();
-		let mut descriptors = ADJECTIVE_DESCRIPTORS.to_vec();
-		
-		sea_list.extend(&SEA_CREATURES);
-		descriptors.extend(&SIZE_DESCRIPTORS);
-	
-		let mut descriptors_and_verbs = descriptors.clone();
-		descriptors_and_verbs.extend(&CREATURE_DESCRIPTORS);
-	
-		let rand_noun = random(sea_list.to_vec());
+		let descriptors_and_verbs: Vec<&'static str> = DESCRIPTORS.iter().cloned().chain(CREATURE_DESCRIPTORS.iter().cloned()).collect();
+
+		let rand_noun = random(SEA_LIST.to_vec());
 		let rand_color = random(COLORS.to_vec());
 		
 		let rand_desc: &str;
 		if SEA_CREATURES.contains(&rand_noun) {
 			rand_desc = random(descriptors_and_verbs);
 		} else {
-			rand_desc = random(descriptors);
+			rand_desc = random(DESCRIPTORS.to_vec());
 		}
+	
 
 		if format!("{}{}{}", rand_desc, rand_color, rand_noun).len() <= max_size {
 			return up_format(vec![rand_desc, rand_color, rand_noun])
