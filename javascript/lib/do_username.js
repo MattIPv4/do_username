991ff0b1b637ab6f5ea6c2a0cff7ee42c16b66eb
luiz <cieslakluiz@gmail.com>
2020-10-29 18:47:42 -0300

    exporting private functions in do_username.js

--- javascript/lib/do_username.js
+++ javascript/lib/do_username.js
@@ -11,19 +11,19 @@ const {
 
 const random = (items) => items[Math.random() * items.length | 0];
 
-const randomNoun = () => random(SEA_LIST);
+const randomNoun = module.exports.randomNoun = () => random(SEA_LIST);
 
-const randomDescriptor = (noun) => {
+const randomDescriptor = module.exports.randomDescriptor = (noun) => {
     if (!SEA_CREATURES.includes(noun)) return random(DESCRIPTORS);
 
     return random(DESCRIPTORS.concat(CREATURE_DESCRIPTORS));
 };
 
-const format = (string) => string[0].toUpperCase() + string.slice(1);
+const format = module.exports.format = (string) => string[0].toUpperCase() + string.slice(1);
 
-const randomColor = () => random(COLORS);
+const randomColor = module.exports.randomColor = () => random(COLORS);
 
-const combineUsername = (maxSize, descriptor, color, noun) => {
+const combineUsername = module.exports.combineUsername = (maxSize, descriptor, color, noun) => {
     if ((descriptor + color + noun).length <= maxSize)
         return descriptor + color + noun;
     else if ((descriptor + noun).length <= maxSize)
